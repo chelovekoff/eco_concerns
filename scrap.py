082551c75179f20d697c84cefec1890cfb592dd1
Aleksandr Chernykh <121977819+chelovekoff@users.noreply.github.com>
2023-02-19 16:14:45 +0100

    Update scrap.py
    two plots were added

--- scrap.py
+++ scrap.py
@@ -4,6 +4,9 @@ from fake_useragent import UserAgent
 import json
 import numpy as np
 import pandas as pd
+!pip install wordcloud
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def ua():
     return(UserAgent().random)
@@ -17,7 +20,7 @@ headers = {'User-Agent': ua()}
 #                '-' : 'другие'}
 
 #===================================for only one word===================================:
-word = 'экология'
+word = 'экология' #'ecology' in Russian
 z = requests.get('https://stateduma.api.dekoder.org/term/ru/' + word, headers = headers)
 #----------------adapted from Nikita Kharakhnin 03.02.2022-----------------
 z1 = z.json()
@@ -30,6 +33,7 @@ df.set_index('Date', inplace = True)
 print(df.head(3))
 
 #===================================for different words===================================:
+#Russian words mentioned during deliberations in the State Duma and associated with environmental issues:
 words = ('экология', 'климат', 'климатический', 'экологическая безопасность', 'экологическая проблема', 'экологически чистый',
          'загрязнение', 'экосистема', 'окружающая среда', 'биоресурсы', 'озоновый', 'вырубка лес', 'отходы',
          'мусор', 'мусорный', 'мусороперерабатывающий', 'мусоросжигательный', 'мусоропереработка', 'свалка', 'техногенный',
@@ -48,8 +52,31 @@ for j in words:
     eco_df = pd.concat([eco_df, df], axis=1)
     del df
 eco_df = eco_df.fillna(0)
-eco_df['sum'] = eco_df.sum(axis=1)
-print(eco_df.iloc[0:5,-5:])
-
-eco_df['sum'].plot()
+eco_df['total'] = eco_df.sum(axis=1)
+print(eco_df.iloc[0:3,-6:])
 eco_df.to_excel("eco_duma.xlsx")
+#===========================================================================================
+
+#------The plot for the total number of environmental concern mentions each year:
+plt.figure(figsize=(18,4))
+plt.bar(x = eco_df.index, height=eco_df['total'])
+plt.title('Total number of environmental concern')
+plt.ylabel('mentions')
+plt.xticks(np.arange(1994, 2022, 1))
+plt.show()
+
+#-----------------The plot for the word map of environmental concerns, 2014-2021:
+eco_sum = pd.DataFrame(eco_df.iloc[:,:-1].sum(axis=0))
+eco_sum.columns = ['total']
+
+#Phrases as a single word:
+for el in eco_sum.index:
+  new_word = el.replace(' ', '_')
+  eco_sum = eco_sum.rename(index={el: new_word})
+
+text = eco_sum.to_string(header=None)
+wordcloud = WordCloud().generate(str(text))
+plt.figure(figsize=(15,10))
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
